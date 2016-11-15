aed8a3559000154d57dc97e45b230812944c5573
vasanth <v123456789>
2016-11-15 10:47:42 +0800

    Oven Test

---
+++ Oven_Test2/Oven_Test2/DataContextSample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oven_Test2
+{
+  class DataContextSample
+  {
+    public DataContextSample()
+    {
+      
+    }
+  }
+}
